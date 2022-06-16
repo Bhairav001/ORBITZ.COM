830af73983b0ccaa2f1146b5e025c85f72a1aa47
Omkar09090 <omkarsalunke1010@gmail.com>
2022-06-16 22:45:41 +0530

    added my all files

---
+++ Omkar/flights.js
@@ -0,0 +1,96 @@
+// Rajkot to Pun
+let flights=[
+
+    // Rajkot to Pun
+    {time:"3:20pm - 7:00am",
+    destination:"Rajkot to Pune",
+    price:$346,
+    airLine_Name:"Air India",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/AI_sq.svg"},
+   
+    {time:"3:20pm - 12:10pm",
+    destination:"Rajkot to Pune",
+    price:$346,
+    airLine_Name:"Air India",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/AI_sq.svg"},
+   
+    {time:"4:00pm - 2:05am",
+    destination:"Rajkot to Pune",
+    price:$909,
+    airLine_Name:"Hahn Air Systems",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/H1_sq.svg"},
+    
+    {time:"8:05am - 10:35pm",
+    destination:"Rajkot to Pune",
+    price:$923,
+    airLine_Name:"Hahn Air Systems",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/H1_sq.svg"},
+
+    
+    // Delhi to Bengalur
+
+    {time:"6:00am - 8:35am",
+    destination:"Delhi to Bengaluru",
+    price:$223,
+    airLine_Name:"IndiGo",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/6E_sq.svg"},
+
+    {time:"8:05am - 10:35pm",
+    destination:"Delhi to Bengaluru",
+    price:$923,
+    airLine_Name:"Hahn Air Systems",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/H1_sq.svg"},
+
+    {time:"8:05am - 10:35pm",
+    destination:"Delhi to Bengaluru",
+    price:$223,
+    airLine_Name:"Air India",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/AI_sq.svg"},
+
+    {time:"8:05am - 10:35pm",
+    destination:"Delhi to Bengaluru",
+    price:$223,
+    airLine_Name:"IndiGo",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/6E_sq.svg"},
+
+    {time:"8:05am - 10:35pm",
+    destination:"Delhi to Bengaluru",
+    price:$223,
+    airLine_Name:"IndiGo",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/6E_sq.svg"},
+
+    //Chennai to Surat
+
+
+    {time:"1:30pm - 9:00pm",
+    destination:"Chennai to Surat",
+    price:$185,
+    airLine_Name:"IndiGo",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/6E_sq.svg"},
+
+    {time:"8:45am - 4:20pm",
+    destination:"Chennai to Surat",
+    price:$185,
+    airLine_Name:"IndiGo",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/6E_sq.svg"},
+
+    {time:"8:10am - 6:35pm",
+    destination:"Chennai to Surat",
+    price:$223,
+    airLine_Name:"Air India",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/AI_sq.svg"},
+
+    {time:"8:05am - 10:35pm",
+    destination:"Chennai to Surat",
+    price:$923,
+    airLine_Name:"Hahn Air Systems",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/H1_sq.svg"},
+
+    {time:"9:05am - 11:35pm",
+    destination:"Chennai to Surat",
+    price:$185,
+    airLine_Name:"IndiGo",
+    pic:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/6E_sq.svg"}
+    
+
+]
---
+++ Omkar/flights_hotel.js
@@ -0,0 +1,132 @@
+let flight_PlaceToStay=[
+    
+    //Rajkot to Pune
+    {pick:"https://a.travel-assets.com/media/meso_cm/PAPI/Images/hotels/7000000/6160000/6152800/6152720/99c1be83_b.jpg",
+    name:"Doubletree By Hilton Pune - Chinchwad",
+    Guests_rated_this_property:"4.6/5 for cleanliness",
+    rating:"4.5/5 Wonderful",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$444 ,
+    Trip_details:"Stay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    {pick:"https://images.trvl-media.com/hotels/1000000/450000/447300/447235/f9c671be.jpg?impolicy=resizecrop&rw=1200&ra=fit",
+    name:"Pride Hotel",
+    Guests_rated_this_property:"3.5/5 for cleanliness",
+    rating:"3.7/5 Good",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$436 ,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    {pick:"https://images.trvl-media.com/hotels/5000000/4520000/4517500/4517481/caea7d1e.jpg?impolicy=resizecrop&rw=598&ra=fit",
+    name:"Crowne Plaza Pune City Centre, an IHG Hotel",
+    Guests_rated_this_property:"4.2/5 for cleanliness.",
+    rating:"4.1/5 Very Good",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$453 ,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    {pick:"https://images.trvl-media.com/hotels/1000000/590000/581400/581366/5ae0a22b.jpg?impolicy=resizecrop&rw=1200&ra=fit",
+    name:"Blue Diamond - IHCL SeleQtions",
+    Guests_rated_this_property:"4.5/5 for cleanliness.",
+    rating:"4.4/5 Excellent",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$460 ,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    {pick:"https://images.trvl-media.com/hotels/4000000/3880000/3877900/3877863/303131e2.jpg?impolicy=resizecrop&rw=598&ra=fit",
+    name:"JW Marriott Hotel Pune",
+    Guests_rated_this_property:"4.7/5 for cleanliness.",
+    rating:"4.6/5 Wonderfult",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$533 ,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    //Delhi to Bengaluru 
+
+    {pick:"https://images.trvl-media.com/hotels/24000000/23090000/23088600/23088585/e9256766.jpg?impolicy=resizecrop&rw=1200&ra=fit",
+    name:"Sheraton Grand Bengaluru Whitefield",
+    Guests_rated_this_property:"4.8/5 for cleanliness.",
+    rating:"4.7/5 Exceptional",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$386 ,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    {pick:"https://images.trvl-media.com/hotels/27000000/26560000/26554200/26554165/a0118907.jpg?impolicy=resizecrop&rw=598&ra=fit",
+    name:"Four Seasons Hotel Bengaluru at Embassy One ",
+    Guests_rated_this_property:"4.9/5 for cleanliness.",
+    rating:"4.6/5 Wonderful",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$457 ,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    {pick:"https://images.trvl-media.com/hotels/13000000/12250000/12246300/12246261/e96784b2.jpg?impolicy=resizecrop&rw=297&ra=fit",
+    name:"Shangri-La Bengaluru",
+    Guests_rated_this_property:"4.8/5 for cleanliness.",
+    rating:"4.7/5 Exceptional",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$401,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    {pick:"https://images.trvl-media.com/hotels/7000000/6470000/6468000/6467933/0682aa84.jpg?impolicy=resizecrop&rw=598&ra=fit",
+    name:"The Ritz-Carlton, Bangalore",
+    Guests_rated_this_property:"4.8/5 for cleanliness.",
+    rating:"4.7/5 Exceptional",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$451,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    {pick:"https://images.trvl-media.com/hotels/1000000/690000/687800/687790/e26dd1fb.jpg?impolicy=resizecrop&rw=1200&ra=fit",
+    name:"The Oterra Bengaluru Electronics City",
+    Guests_rated_this_property:"4.5/5 for cleanliness.",
+    rating:"4.3/5 Excellent",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$350,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    //channai to surat
+
+    {pick:"https://images.trvl-media.com/hotels/1000000/690000/687800/687790/81e113d8.jpg?impolicy=resizecrop&rw=598&ra=fit",
+    name:"Surat Marriott Hotel",
+    Guests_rated_this_property:"4.4/5 for cleanliness.",
+    rating:"4.3/5 Excellent",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$247,
+    Trip_details:"Stay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    {pick:"https://images.trvl-media.com/hotels/20000000/19870000/19861000/19860953/27dfd492.jpg?impolicy=resizecrop&rw=1200&ra=fit",
+    name:"Courtyard by Marriott Surat",
+    Guests_rated_this_property:"4.6/5 for cleanliness.",
+    rating:"4.2/5 Very Good",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$236,
+    Trip_details:"Stay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+    
+    {pick:"https://images.trvl-media.com/hotels/71000000/70980000/70979500/70979425/3407c978.jpg?impolicy=resizecrop&rw=1200&ra=fit",
+    name:"7 Orbit Hotel Surat",
+    Guests_rated_this_property:"5.0/5 for cleanliness.",
+    rating:"5.0/5 Exceptional",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$180,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    {pick:"https://images.trvl-media.com/hotels/16000000/15810000/15802700/15802674/bf7d83ce.jpg?impolicy=resizecrop&rw=1200&ra=fit",
+    name:"Hotel Sifat International",
+    Guests_rated_this_property:"4.4/5 for cleanliness.",
+    rating:"4.2/5 Very Good",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$189,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+    {pick:"https://images.trvl-media.com/hotels/17000000/16760000/16758900/16758843/b082bdc9.jpg?impolicy=resizecrop&rw=1200&ra=fit",
+    name:"Hotel Orange International",
+    Guests_rated_this_property:"4.6/5 for cleanliness.",
+    rating:"4.4/5 Excellent",
+    logoPool:"https://cdn.pixabay.com/photo/2017/01/31/20/15/pictogram-2026956_960_720.png",presentPool:"Pool",
+    price:$190,
+    Trip_details:"tay (1 night) + Flight (1 roundtrip ticket), taxes & fees ,per traveler (includes 1 room)"},
+
+
+
+
+
+]
