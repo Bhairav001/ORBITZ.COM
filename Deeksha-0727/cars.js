7d904f8cd5e3e17f8833cf43302e082b9522e4e7
Deekshachandan <deeksharajpurohit15@gmail.com>
2022-06-18 19:26:32 +0530

    added all the pages again

---
+++ Deeksha-0727/cars.js
@@ -0,0 +1,63 @@
+
+
+
+
+let cars=[
+    {name: "ford figo", 
+    pic:"https://mediaim.expedia.com/cars/41/01524247-e521-4941-a996-5840166aa2a9.jpg?impolicy=resizecrop&ra=fit&rh=165&rw=165",
+class:"economy",
+price:$125},
+
+
+{name: "Hyundai Grand", 
+pic:"https://mediaim.expedia.com/cars/41/322b9191-337a-4f57-b168-2bc78a963554.jpg?impolicy=resizecrop&ra=fit&rh=165&rw=165",
+class:"Mini",
+price:$35},
+
+
+{name: "Maruthi Swift", 
+pic:"https://mediaim.expedia.com/cars/41/bd92f20c-853a-4d39-a571-fdf2d256dae3.jpg?impolicy=resizecrop&ra=fit&rh=165&rw=165",
+class:"Compact",
+price:$110},
+
+
+{name: "Toyota Etios", 
+pic:"https://mediaim.expedia.com/cars/41/fc1eaef9-1bd7-407f-9309-777b9c9a1ae2.jpg?impolicy=resizecrop&ra=fit&rh=165&rw=165",
+class:"Midsize",
+price:$55},
+
+
+{name: "Honda City", 
+pic:"https://mediaim.expedia.com/cars/41/7513a00e-5fdd-4c58-8c9c-af1103be87bf.jpg?impolicy=resizecrop&ra=fit&rh=165&rw=165",
+class:"Luxury",
+price:$125},
+
+
+{name: "Mahindra Scorpio", 
+pic:"https://mediaim.expedia.com/cars/41/4599604d-6c8f-4485-b7b3-b354301de9b8.jpg?impolicy=resizecrop&ra=fit&rh=165&rw=165",
+class:"Luxury SUV",
+price:$59},
+
+
+{name: "Hyundai Creta", 
+pic:"https://mediaim.expedia.com/cars/41/c3c4c6aa-7938-4a35-a0f5-b28f7710d0e5.jpg?impolicy=resizecrop&ra=fit&rh=165&rw=165",
+class:"Premium SUV",
+price:$67},
+
+
+{name: "Mahindra XUV - 500", 
+pic:"https://mediaim.expedia.com/cars/41/dcf776f0-b661-4f08-86a9-185e0bea3362.jpg?impolicy=resizecrop&ra=fit&rh=165&rw=165",
+class:"Fullsize SUV",
+price:$75},
+
+{name: "Toyota Camry", 
+pic:"https://mediaim.expedia.com/cars/41/73fd0a44-aea1-4f2c-92f3-7e1fccdd2385.jpg?impolicy=resizecrop&ra=fit&rh=165&rw=165",
+class:"Midsize",
+price:$144},
+
+{name: "Toyota Innova ", 
+pic:"https://mediaim.expedia.com/cars/41/6a7c63a9-f733-47ef-bbf1-1800ed3ad1a7.jpg?impolicy=resizecrop&ra=fit&rh=165&rw=165",
+class:"Fullsize Van",
+price:$73},
+]
+
---
+++ Deeksha-0727/stays.js
@@ -0,0 +1,65 @@
+
+
+//  Mumbai to delhi
+// there are 10 objects in stays
+
+
+let  stay=[
+    { image:"https://images.trvl-media.com/hotels/8000000/7250000/7242500/7242492/0d763157.jpg?impolicy=resizecrop&rw=455&ra=fit",
+     name:"Sangri lo Eros",
+      Price:$127, 
+      Rating:4.5/5},
+    
+    { image:"https://a.travel-assets.com/media/meso_cm/PAPI/Images/hotels/4000000/3860000/3851700/3851663/958ad657_b.jpg",
+      name:"Leela Palace",
+       Price:$292, 
+       Rating:4.6/5},
+     
+    { image:"https://images.trvl-media.com/hotels/2000000/1070000/1063100/1063007/eca8a680.jpg?impolicy=resizecrop&rw=455&ra=fit",
+       name:"Trident Gurga",
+        Price:$125, 
+        Rating:4.5/5},
+      
+    { image:"https://images.trvl-media.com/hotels/16000000/15240000/15233900/15233833/b02869f0.jpg?impolicy=resizecrop&rw=455&ra=fit",
+        name:"Haveli Dharampura",
+         Price:$, 
+         Rating:4.5/5},
+       
+    { image:"https://images.trvl-media.com/hotels/7000000/6190000/6184300/6184205/8d5c5144.jpg?impolicy=resizecrop&rw=455&ra=fit",
+         name:"Boolrooms",
+          Price:$127, 
+          Rating:4.5/5},
+        
+    { image:"https://images.trvl-media.com/hotels/3000000/2220000/2215100/2215082/c55ae853.jpg?impolicy=resizecrop&rw=455&ra=fit",
+          name:"Prime Balaji Delux",
+           Price:$297, 
+           Rating:4.5/5},
+         
+    
+     { image:"https://images.trvl-media.com/hotels/9000000/8740000/8738300/8738241/9fdc0ad2.jpg?impolicy=resizecrop&rw=455&ra=fit",
+           name:"The oberoi Gurgaon",
+            Price:$115, 
+            Rating:4.5/5},
+          
+    { image:"https://images.trvl-media.com/hotels/9000000/8740000/8738300/8738241/9fdc0ad2.jpg?impolicy=resizecrop&rw=455&ra=fit",
+     name:"Vivantha New Delhi",
+      Price:$230, 
+      Rating:4.5/5},
+    
+    
+      { image:"https://images.trvl-media.com/hotels/6000000/5650000/5648500/5648455/479ab915.jpg?impolicy=resizecrop&rw=455&ra=fit",
+      name:"Hotel City star",
+       Price:$193, 
+       Rating:4.5/5},
+     
+       { image:"https://images.trvl-media.com/hotels/7000000/6390000/6382300/6382233/f90787dd.jpg?impolicy=resizecrop&rw=455&ra=fit",
+       name:"IGH Hotel",
+        Price:$145, 
+        Rating:4.5/5},
+    
+    
+    
+    ]
+    
+    
+    
